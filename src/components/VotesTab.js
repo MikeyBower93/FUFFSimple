f8a44ad10c7698cd238ce9f4a8d8407ce0e0c69e
MikeyBower93 <44525935+MikeyBower93@users.noreply.github.com>
2019-03-09 10:35:21 +0000

    Restaurant item sub item with avatars.

--- src/components/VotesTab.js
+++ src/components/VotesTab.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import { View, FlatList, Text } from 'react-native'; 
-   
+import { View, FlatList, Text, Image } from 'react-native'; 
+import cuid from 'cuid'  
+import _  from 'lodash' 
+
 class VotesTab extends Component {    
   constructor(props) { 
     super(props);
@@ -9,11 +11,27 @@ class VotesTab extends Component {
         Store styling variables and headers into navigation folder etc. 
      */
     this.state = {
-        RestaurantOptions: [{id: 1, name: "Restaurant 1"}, 
-        { id: 2, name :"Restaurant 2" }, 
-        { id: 3, name :"Restaurant 3" }] 
+        RestaurantOptions: [this.generateRestaurantOption(1, "Minerva", 3),
+            this.generateRestaurantOption(2, "Berts", 2),
+            this.generateRestaurantOption(3, "Atom", 1)] 
     }; 
   }  
+
+  generateRestaurantOption(id, name, avatarCount) {  
+      
+    var avatars = _.times(avatarCount, (number) => {
+        var newId = cuid();
+        return {
+            uri: `https://api.adorable.io/avatars/285/'${newId}.png`
+        }
+    });
+
+    return {
+        id: id,
+        name: name,
+        avatars: avatars
+    }
+  }
 
   render() {
     return (
@@ -21,7 +39,7 @@ class VotesTab extends Component {
         <FlatList 
             data={this.state.RestaurantOptions}
             renderItem={({item, index}) => (
-                <RestaurantItem title={item.name}></RestaurantItem>
+                <RestaurantItem title={item.name} avatars={item.avatars}></RestaurantItem>
             )}
         />
       </View>
@@ -29,10 +47,40 @@ class VotesTab extends Component {
   }  
 }
 
-const RestaurantItem = ({title}) => (
+class RestaurantItem extends Component {
+    constructor(props) { 
+        super(props);  
+      }  
+
+      render() {
+          return (
+            <View>
+                <Text>Yeah Brother! - {this.props.title}, {this.props.avatars.length}</Text>
+                <FlatList
+                    data={this.props.avatars}
+                    renderItem={({item, index}) => (
+                        <Image
+                            style={{width: 50, height: 50}}
+                            source={{uri: item.uri }}></Image>
+                    )}/>
+            </View>
+          );
+      }
+}
+
+/*
+const RestaurantItem = ({title, avatars}) => (
     <View>
-        <Text>Yeah Brother! - {title}</Text>
+        <Text>Yeah Brother! - {title}, {avatars}</Text>
+        <FlatList
+            data={avatars}
+            renderItem={({item, index}) => (
+                <Image
+                    style={{width: 50, height: 50}}
+                    source={{uri: item.uri }}></Image>
+            )}/>
     </View>
   );
+*/
 
 export default VotesTab;
