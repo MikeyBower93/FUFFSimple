0191d451e9ede67ed9b3a667667b251c14fe28c4
MikeyBower93 <44525935+MikeyBower93@users.noreply.github.com>
2019-03-13 20:54:38 +0000

    Stylings with percentages.

--- src/components/FUFFButtonComponent.js
+++ src/components/FUFFButtonComponent.js
@@ -7,52 +7,64 @@ export default class FuffButtonComponent extends Component {
     }  
     
     render() {
-        return (
-        <View style={styles.outerView}>
-          <TouchableOpacity onPress={this.props.buttonClicked}>
-            <View style={styles.outerButton}>
-              <View style={styles.innerButton} onPress={this.props.buttonClicked}>
-                <Text style={{marginLeft: 15, fontSize: 20, color:'black'}}>{this.props.text}</Text>  
-              </View>  
-              <View style={{flex:1, flexDirection: 'row', justifyContent:'flex-end', marginRight: 5}}>
-                {this.props.children} 
+        return ( 
+          <View>
+            <TouchableOpacity 
+              style={styles.outerContainer} 
+              onPress={this.props.buttonClicked}>
+              <View 
+                style={styles.innerContainer} />
+              <View style={styles.textContainer}>
+                <Text style={styles.buttonText}>{this.props.text}</Text>  
               </View>
-            </View> 
-          </TouchableOpacity>
-        </View>
+              <View style={styles.additionalsContainer}>
+                {this.props.children} 
+              </View>          
+            </TouchableOpacity>
+          </View>  
         );
     } 
 }
 
-const styles = StyleSheet.create({
-  outerView: {
-    backgroundColor: "#ffffff",
+const styles = StyleSheet.create({ 
+  outerContainer: {
+    width:'100%',  
     borderRadius: 5,
+    flex:1,
+    flexDirection: 'row',
+    alignItems:'center', 
+    backgroundColor: "#ffffff",
     shadowColor: "#000000",
-    shadowOpacity: 0.8,
+    shadowOpacity: 0.1,
     shadowRadius: 5,
     shadowOffset: {
       height: 1,
       width: 0
     },
-    elevation: 2,
+    elevation: 1,
   },
-  outerButton: {
-    width:'100%', 
-    height:75, 
-    borderRadius: 5,
-    flex:1,
-    flexDirection: 'row',
-    alignItems:'center'
-  },
-  innerButton: { 
-    backgroundColor:'yellow',
-    width:'80%',
-    height:75, 
-    borderRadius: 5,
-    flex: 1, 
+  textContainer: {
+    position:'absolute', 
+    flex:1, 
     flexDirection: 'row', 
-    justifyContent:'flex-start',
-     alignItems:'center'
+    justifyContent:'flex-start'
+  },
+  buttonText: {
+    marginLeft: 15, 
+    fontSize: 20, 
+    color:'black'
+  },
+  additionalsContainer: {
+    position:'absolute', 
+    marginLeft:'80%' , 
+    flex:1,
+    flexDirection: 'row', 
+    justifyContent:'flex-end'
+  },
+  innerContainer: { 
+    backgroundColor:'yellow',
+    width:'70%',
+    height:75, 
+    borderRadius: 5
   }
 });
