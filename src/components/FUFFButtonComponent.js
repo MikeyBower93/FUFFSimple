9d44c68835f84b58b66a16689071e3809102cb5e
MikeyBower93 <44525935+MikeyBower93@users.noreply.github.com>
2019-03-13 20:02:34 +0000

    Buttons fully aligned.

--- src/components/FUFFButtonComponent.js
+++ src/components/FUFFButtonComponent.js
@@ -12,11 +12,11 @@ export default class FuffButtonComponent extends Component {
           <TouchableOpacity onPress={this.props.buttonClicked}>
             <View style={styles.outerButton}>
               <View style={styles.innerButton} onPress={this.props.buttonClicked}>
-                <Text style={{marginLeft: 15, fontSize: 20, color:'black', flex:1, flexDirection: 'row', justifyContent:'flex-start'}}>{this.props.text}</Text>  
-                <View style={{flex:1, flexDirection: 'row', justifyContent:'flex-end'}}>
-                  {this.props.children} 
-                </View>
+                <Text style={{marginLeft: 15, fontSize: 20, color:'black'}}>{this.props.text}</Text>  
               </View>  
+              <View style={{flex:1, flexDirection: 'row', justifyContent:'flex-end', marginRight: 5}}>
+                {this.props.children} 
+              </View>
             </View> 
           </TouchableOpacity>
         </View>
@@ -40,15 +40,19 @@ const styles = StyleSheet.create({
   outerButton: {
     width:'100%', 
     height:75, 
-    borderRadius: 5
-  },
-  innerButton: { 
-    backgroundColor:'yellow',
-     width:'80%',
-     height:75, 
     borderRadius: 5,
     flex:1,
     flexDirection: 'row',
     alignItems:'center'
+  },
+  innerButton: { 
+    backgroundColor:'yellow',
+    width:'80%',
+    height:75, 
+    borderRadius: 5,
+    flex: 1, 
+    flexDirection: 'row', 
+    justifyContent:'flex-start',
+     alignItems:'center'
   }
 });
